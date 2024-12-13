b82981d42e4fe440c6c3e565184c2271e42221b3
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-12-13 15:17:13 +0000

    Move folders around

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,10 +1,10 @@
 const { UnauthorizedError } = require("../errors/errors");
-// const moment = require('moment')
+const moment = require("moment");
 
 const errorHandler = (err, req, res, next) => {
-  // if (process.env.NODE_ENV === 'development') {
-  //   console.log(moment().format('MMMM Do YYYY, h:mm:ss a'), err);
-  // }
+  if (process.env.NODE_ENV === "development") {
+    console.log(moment().format("MMMM Do YYYY, h:mm:ss a"), err);
+  }
 
   const statusCode = err.statusCode || 500;
   let message = err.message;
