07de91f3cb8539c78830068116e10af3efb7e9e7
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-12-13 15:06:46 +0000

    Fix audit problems

--- config/db.js
+++ config/db.js
@@ -1,22 +1,23 @@
 // Connection function to MongoDb, called on server.js
 
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 const connectDb = () => {
   return new Promise((resolve, reject) => {
     mongoose
-      .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(process.env.MONGO_URI)
       .then((db) => {
-        console.log('-------------------------------------------------------------'.cyan);
+        console.log(
+          "-------------------------------------------------------------".cyan
+        );
         console.log(`MongoDB Connected: ${db.connection.host}`.cyan);
         resolve(db);
       })
       .catch((err) => {
-        console.error(`Error connecting to MongoDB: ${err.message}`.red.underline.bold);
+        console.error(
+          `Error connecting to MongoDB: ${err.message}`.red.underline.bold
+        );
         reject(err);
       });
   });
--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,16 +1,16 @@
-const { UnauthorizedError } = require('../errors/errors');
-const moment = require('moment')
+const { UnauthorizedError } = require("../errors/errors");
+// const moment = require('moment')
 
 const errorHandler = (err, req, res, next) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'), err);
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   console.log(moment().format('MMMM Do YYYY, h:mm:ss a'), err);
+  // }
 
   const statusCode = err.statusCode || 500;
   let message = err.message;
 
   if (statusCode === 500) {
-    message = 'An unexpected error occurred, please try again later.';
+    message = "An unexpected error occurred, please try again later.";
   }
   res.status(statusCode);
 
@@ -22,7 +22,7 @@ const errorHandler = (err, req, res, next) => {
   } else {
     res.status(statusCode).json({
       message: message,
-      stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
     });
   }
 };
