81ef71098c9a914745625f719dc87ee7ee1a18de
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-06-02 22:12:55 +0100

    Populate the account details in bills

--- src/features/bills/controllers/getBills.controller.ts
+++ src/features/bills/controllers/getBills.controller.ts
@@ -5,9 +5,11 @@ import { IUser } from "../../users/model/user.model";
 const getBills = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = req.user as IUser;
-    const bills = await Bill.find({ createdBy: user._id }).sort({
-      order: 1,
-    });
+    const bills = await Bill.find({ createdBy: user._id })
+      .populate("account")
+      .sort({
+        order: 1,
+      });
     res.status(200).json(bills);
   } catch (err) {
     next(err);
