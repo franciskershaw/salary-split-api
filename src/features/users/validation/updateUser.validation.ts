dcea0c7e6c15c3fe75bbcbcee889e45439b3723e
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-06-10 21:10:53 +0100

    Fix schema

--- src/features/users/validation/updateUser.validation.ts
+++ src/features/users/validation/updateUser.validation.ts
@@ -6,13 +6,19 @@ const updateUserSchema = joi.object({
     firstName: joi.string().required(),
     lastName: joi.string().required(),
   }),
-  salary: joi.number().required().min(0).max(1000000).precision(2).messages({
-    "number.base": "Salary must be a number.",
-    "number.min": "Salary cannot be negative.",
-    "number.max": "Salary cannot exceed 1,000,000.",
-    "number.precision": "Salary can only have up to 2 decimal places.",
-    "any.required": "Salary is required.",
-  }),
+  takeHomePay: joi
+    .number()
+    .required()
+    .min(0)
+    .max(1000000)
+    .precision(2)
+    .messages({
+      "number.base": "Salary must be a number.",
+      "number.min": "Salary cannot be negative.",
+      "number.max": "Salary cannot exceed 1,000,000.",
+      "number.precision": "Salary can only have up to 2 decimal places.",
+      "any.required": "Salary is required.",
+    }),
   defaultCurrency: joi
     .string()
     .required()
