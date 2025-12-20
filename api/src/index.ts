3bed38dd5f43af25bce4797484690976b4528048
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-12-20 10:35:08 +0000

    Port over most of the original server file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,36 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDb from "./core/config/databse";
+import { securityHeaders } from "./core/middleware/security.middleware";
+
+const isNetworkDevelopmentMode =
+  process.env.NODE_ENV === "development" && process.argv.includes("--host");
 
 const PORT = Number(process.env.PORT) || 3000;
 const app = new Hono();
 
+app.use(logger());
+
+app.use(securityHeaders);
+
+app.use(
+  cors({
+    origin: isNetworkDevelopmentMode
+      ? (process.env.CORS_ORIGIN_NETWORK as string)
+      : (process.env.CORS_ORIGIN as string),
+    credentials: true,
+  })
+);
+
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.json(
+    { messge: "Welcome to the Salary Split API (Hono Edition)" },
+    200
+  );
 });
 
 // Start everything
